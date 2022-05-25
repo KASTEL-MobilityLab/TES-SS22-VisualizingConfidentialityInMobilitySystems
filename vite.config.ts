925e1eae03c1f4c3f96c660b805479d30628aa35
Moritz Mistol <50528376+MoritzM00@users.noreply.github.com>
2022-05-25 18:19:54 +0200

    Automatic Deployment to Github Pages (#28)
    * ci: add lint script without --fix
    
    * ci: add deploy job to build workflow
    
    * ci: set base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { fileURLToPath, URL } from "url";
-
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/TES-SS22-VisualizingConfidentialityInMobilitySystems/",
   plugins: [vue()],
   resolve: {
     alias: {
