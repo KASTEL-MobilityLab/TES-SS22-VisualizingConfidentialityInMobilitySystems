7c90282ab8b81b97cf96bebcfa6e16a2d1f50a03
Moritz Mistol <moritz.mistol@gmail.com>
2022-05-26 18:07:18 +0200

    refactor: delete unnecessary template code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Here will be the map later on</h1>
   </main>
 </template>
