96a27e3227e3149f8ccc11c69911e1193889e490
Moritz Mistol <moritz.mistol@gmail.com>
2022-05-26 18:33:40 +0200

    chore: install and import bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
