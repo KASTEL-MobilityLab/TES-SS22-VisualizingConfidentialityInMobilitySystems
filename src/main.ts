db340037d4f39c4d764c39bdcd173201654002a2
Moritz Mistol <moritz.mistol@gmail.com>
2022-05-26 18:48:46 +0200

    chore: install leaflet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "leaflet/dist/leaflet.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
