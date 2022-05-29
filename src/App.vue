d092a4adc244cd5bf288612a264867fcf0fed75e
Moritz Mistol <moritz.mistol@gmail.com>
2022-05-29 12:09:21 +0200

    feat: add components and views

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,42 @@
 <script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
-import { RouterLink, RouterView } from "vue-router";
+import MapComponent from "@/components/MapComponent.vue";
+import NavBar from "@/components/NavBar.vue";
+import DataViewer from "./components/DataViewer.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <main>
+    <NavBar />
+    <div class="outer-wrap">
+      <MapComponent />
+      <div class="overlay">
+        <DataViewer />
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </main>
 </template>
-
-<style>
-@import "@/assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+<style scoped>
+.outer-wrap {
+  /* Must have some sort of height and width so that
+    inner children (map and overlay) can position
+    themselves to it */
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  z-index: 0;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+div.overlay {
+  /* This stretches these elements to the edges of
+    the closest element upwards in the DOM with a relative
+    positioning (in this case it's the .outer-wrap) */
+  position: absolute;
+  top: 15vh;
+  right: 0;
+  bottom: 0;
+  left: 65vw;
+  z-index: 2;
+  opacity: 0.9;
+  border-radius: 150px;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <RouterLink to="/" class="navbar-brand"
+        >Confidentiality Visualizer</RouterLink
+      >
+      <ul class="navbar-nav ms-auto me-auto">
+        <li class="nav-item active dropdown">
+          <a
+            id="navbarDropdown"
+            class="nav-link active dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+          >
+            User
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">User1</a></li>
+            <li><a class="dropdown-item" href="#">User2</a></li>
+            <li><a class="dropdown-item" href="#">User3</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <span class="nav-link active">City</span>
+        </li>
+        <li class="nav-item">
+          <span class="nav-link active">Company</span>
+        </li>
+      </ul>
+      <div class="d-flex">
+        <span class="navbar-brand p-2">DE | EN</span>
+        <div class="vr"></div>
+        <RouterLink to="/help" class="navbar-brand p-2">Help</RouterLink>
+      </div>
+    </div>
+    <RouterView />
+  </nav>
+</template>
