7dc9834bc7aa3429d102ca3bf205dcacc7a5c7da
Moritz Mistol <moritz.mistol@gmail.com>
2022-05-29 12:07:06 +0200

    chore: import bootstrap and leaflet

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "leaflet/dist/leaflet.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
