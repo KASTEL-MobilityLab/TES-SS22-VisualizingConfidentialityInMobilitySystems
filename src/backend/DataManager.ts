d935182126cf0a317402e6612f777ea07deab441
Moritz Mistol <moritz.mistol@gmail.com>
2022-06-02 15:55:04 +0200

    feat: integrate DataLoader into DataManager

--- src/backend/DataManager.ts
+++ src/backend/DataManager.ts
@@ -1,9 +1,29 @@
+import type { Company } from "./dataFields/Company";
+import type { Trip } from "./dataFields/Trip";
+import type { User } from "./dataFields/User";
 import { DataLoader } from "./DataLoader";
 
 export class DataManager {
-  readonly dataLoader: DataLoader;
+  users: User[];
+  companies: Company[];
+  trips: Trip[];
+
+  dataLoader: DataLoader;
+
+  private dataPath = "@/data/";
 
   constructor() {
     this.dataLoader = new DataLoader();
+    this.users = [];
+    this.companies = [];
+    this.trips = [];
+  }
+
+  /**
+   * This method (asnchronously) loads all data into the DataManager.
+   */
+  async loadAllData() {
+    this.users = this.dataLoader.loadUsers();
+    this.trips = this.dataLoader.loadTrips();
   }
 }
