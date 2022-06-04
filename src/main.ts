14bc97ddc4c82f3408beede9df78241ca932ea72
Moritz Mistol <moritz.mistol@gmail.com>
2022-06-04 18:05:44 +0200

    feat(global-DataManager-Instance): provide app-level data manager instance

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "leaflet/dist/leaflet.css";
 import "reflect-metadata";
-import { createApp } from "vue";
+import { createApp, readonly } from "vue";
 import App from "./App.vue";
+import { DataManager } from "./backend/DataManager";
+import { dataManagerKey } from "./keys";
 import router from "./router";
 
 const app = createApp(App);
 
 app.use(router);
+
+/**
+ * provide DataManager instance globally to all components
+ * note: a component that needs access, has to inject it
+ * with `const $dm = inject('$dataManager')`
+ */
+const dm: DataManager = new DataManager();
+app.provide(dataManagerKey, readonly(dm));
 
 app.mount("#app");
