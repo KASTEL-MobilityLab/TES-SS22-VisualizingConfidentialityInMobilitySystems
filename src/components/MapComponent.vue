b17f62da1591c1bedbbad8b43c58342bffd9f3c2
Moritz Mistol <moritz.mistol@gmail.com>
2022-06-06 16:01:58 +0200

    feat(#51): use Stamen.Watercolor tilelayer and set zoom

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -10,10 +10,24 @@ export default {
     let map;
     onMounted(() => {
       map = new L.Map("leafletMap").setView([49.006889, 8.403653], 13);
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution: "© OpenStreetMap",
-      }).addTo(map);
+      L.tileLayer(
+        "https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",
+        {
+          attribution:
+            'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          minZoom: 14,
+          maxZoom: 17,
+        }
+      ).addTo(map);
+      L.tileLayer(
+        "https://stamen-tiles.a.ssl.fastly.net/terrain-labels/{z}/{x}/{y}.png",
+        {
+          attribution:
+            'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          minZoom: 14,
+          maxZoom: 17,
+        }
+      ).addTo(map);
     });
   },
 };
