1e08e301c3d07a756ae730da06b3d95f2b718445
Moritz Mistol <moritz.mistol@gmail.com>
2022-06-09 19:04:55 +0200

    feat(#19): setup project to use i18n plugin

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
+import en from "@/locales/en.json";
 import "@fortawesome/fontawesome-free/js/all.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "leaflet/dist/leaflet.css";
 import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
 import App from "./App.vue";
 import router from "./router";
 
+const i18n = createI18n({
+  locale: "en",
+  messages: {
+    en,
+  },
+});
+
 const app = createApp(App);
 
-app.use(router);
+app.use(router, i18n);
 
 app.mount("#app");
