7c5f4f5a0b7bdb5a61fac382518af7d1e7851d37
Moritz Mistol <moritz.mistol@gmail.com>
2022-06-09 10:42:19 +0200

    refactor: remove create font awesome marker util function

--- src/utils/markerUtils.ts
+++ src/utils/markerUtils.ts
@@ -1,24 +1,9 @@
 import L from "leaflet";
 
 const iconAnchor: L.PointExpression = [24, 24];
-const fontAwesomeCSSName = "fontAwesomeIconLeaflet";
 
-/**
- * A helper function to easily create font awesome icons for use in leaflet markers.
- *
- * @param fontAwesomeClassString a string that contains all font awesome classes of the icon seperated by white space
- * @param iconSize the size of the icon
- * @returns a leaflet DivIcon ready to be used in a marker.
- */
-export function createFontAwesomeIcon(
-  fontAwesomeClassString: string
-): L.DivIcon {
-  return L.divIcon({
-    html: `<i class='${fontAwesomeClassString}'></i>`,
-    iconAnchor: iconAnchor,
-    className: "fontAwesomeIcon",
-  });
-}
+// used for css styling, to center the icon
+const fontAwesomeCSSName = "fontAwesomeIconLeaflet";
 
 export const bikeIcon = L.divIcon({
   html: "<i class='fa-solid fa-bicycle fa-3x'></i>",
