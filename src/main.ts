323778a477cddab1047b3aa2dd122ab06411cbdf
Moritz Mistol <moritz.mistol@gmail.com>
2022-06-13 19:21:41 +0200

    feat: add translations

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import de from "@/locales/de.json";
 import en from "@/locales/en.json";
 import "@fortawesome/fontawesome-free/js/all.js";
 import "bootstrap/dist/css/bootstrap.css";
@@ -9,14 +10,18 @@ import App from "./App.vue";
 import router from "./router";
 
 const i18n = createI18n({
+  legacy: false,
   locale: "en",
+  fallBackLocale: "en",
   messages: {
     en,
+    de,
   },
 });
 
 const app = createApp(App);
 
-app.use(router, i18n);
+app.use(router);
+app.use(i18n);
 
 app.mount("#app");
