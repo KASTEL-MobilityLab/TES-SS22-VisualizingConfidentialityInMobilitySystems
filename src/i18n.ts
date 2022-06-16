0ae15989e5aee532cf47ca20695e7f30fad04da0
Moritz Mistol <moritz.mistol@gmail.com>
2022-06-16 14:09:23 +0200

    chore: merge explanations into locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,12 @@
 import de from "@/locales/de.json";
 import en from "@/locales/en.json";
+import explanationsDE from "@/locales/explanations/explanationsDE.json";
+import explanationsEN from "@/locales/explanations/explanationsEN.json";
 import { createI18n } from "vue-i18n";
+
+// merge explanations into locales (target is en)
+Object.assign(en, explanationsEN);
+Object.assign(de, explanationsDE);
 
 export const i18n = createI18n({
   legacy: false,
