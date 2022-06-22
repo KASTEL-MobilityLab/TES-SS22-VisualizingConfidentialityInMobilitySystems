af4252f8513a93fd099659fbebb1b6781e1237c3
Moritz Mistol <moritz.mistol@gmail.com>
2022-06-22 17:45:58 +0200

    fix(#81): 404 Refresh Error fixed by using WebHashHistory

--- src/views/dataViewer/RiderDataView.vue
+++ src/views/dataViewer/RiderDataView.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import type { DataManager } from "@/backend/DataManager";
-import { dataManagerKey } from "@/keys";
-import { inject } from "vue";
-
 const user = {
   id: "U01",
   forename: "Theo",
@@ -17,12 +13,10 @@ const riskColor = {
   phoneNumber: "text-warning",
   email: "text-warning",
 };
-const $dm = inject(dataManagerKey) as DataManager;
 </script>
 
 <template>
   <h4 class="text-center m-2">Rider Data</h4>
-  <div>{{ $dm.companies }}</div>
   <template v-for="(data, fieldName) in user" :key="fieldName">
     <div :class="riskColor[fieldName]">
       <div class="row m-2 p-2">
