a07a276ed627b7db78b324dc66fa29d6104be9cb
DanialAhmed <danialahmed946@gmail.com>
2022-06-23 14:57:36 +0200

    fix: refactor DataViews

--- src/views/dataViewer/RiderDataView.vue
+++ src/views/dataViewer/RiderDataView.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import { DataModule } from "@/backend/dataModules/DataModule";
-import { RiskManager } from "@/backend/RiskManager";
 import { dataManagerKey } from "@/keys";
 import { inject } from "vue";
 const $dm = inject(dataManagerKey);
-var dmUsers = $dm?.users;
-var dmDataModule = new DataModule(dmUsers![0], $dm?.riskManager);
-const riskManager = new RiskManager();
-const risks = dmDataModule.risks;
-console.log(risks);
+var users = $dm?.users;
+var dataModule = new DataModule(users![0], $dm?.riskManager);
 const riskColor = {
   id: "text-success",
   forename: "text-warning",
@@ -16,19 +12,16 @@ const riskColor = {
   phoneNumber: "text-warning",
   email: "text-warning",
 };
-const testString = "TODO";
-const currentRole = $dm?.currentRole;
-console.log(dmDataModule.displayedData);
 </script>
 
 <template>
   <h4 class="text-center m-2">Rider Data</h4>
-  <template v-for="(value, key) in dmDataModule.displayedData" :key="key">
+  <template v-for="(value, key) in dataModule.displayedData" :key="key">
     <div class="risks[key]">
       <!-- div class="riskColor[key]"> -->
       <div class="row m-2 p-2">
         <div class="col m-2 fw-bold">
-          <button @click="riskManager.getExplanation(testString, currentRole)">
+          <button>
             {{ $t(key) }}
           </button>
         </div>
--- src/views/dataViewer/TripDataView.vue
+++ src/views/dataViewer/TripDataView.vue
@@ -1,23 +1,18 @@
 <script setup lang="ts">
 import { DataModule } from "@/backend/dataModules/DataModule";
-import { RiskManager } from "@/backend/RiskManager";
 import { dataManagerKey } from "@/keys";
 import { inject } from "vue";
 
 const $dm = inject(dataManagerKey);
-const riskManager = new RiskManager();
 var dmUsers = $dm?.users;
 var dmDataModule = new DataModule(dmUsers![0], $dm?.riskManager);
-const testString = "TODO";
-const currentRole = $dm?.currentRole;
-console.log(dmDataModule.risks);
 </script>
 
 <template>
   <div v-for="(value, key) in dmDataModule.displayedData" :key="key">
     <!-- Call method that opens the overlay of the explanation after click-->
-    <button @click="riskManager.getExplanation(testString, currentRole)">
-      {{ key }}
+    <button>
+      {{ $t(key) }}
     </button>
     : {{ value }}
   </div>
