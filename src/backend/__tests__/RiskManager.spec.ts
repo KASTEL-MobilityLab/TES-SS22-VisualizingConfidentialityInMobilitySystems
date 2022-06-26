80dd8d8281aed002c023b017d94f797e310540c3
Moritz Mistol <moritz.mistol@gmail.com>
2022-06-26 13:28:42 +0200

    chore: adapt tests for dataType

--- src/backend/__tests__/RiskManager.spec.ts
+++ src/backend/__tests__/RiskManager.spec.ts
@@ -1,46 +1,21 @@
 import { DataManager } from "@/backend/DataManager";
-import { DataModule } from "@/backend/dataModules/DataModule";
 import "reflect-metadata";
 import { describe, expect, it } from "vitest";
-import { DataLoader } from "../DataLoader";
-import { DataType } from "../dataType";
-import { RiskDefinition } from "../riskManager/RiskDefinition";
-import { RiskLevel } from "../riskManager/RiskLevel";
-import { Role } from "../roles";
+import { DataModule } from "../dataModules/DataModule";
+import * as expectedData from "./data/expectedData";
+import { testDataLoaderParams } from "./DataLoader.spec";
 
-describe.concurrent("TestRisks", async () => {
-  const dl = new DataLoader({
-    /**
-         * TODO: Insert Test Data here
-         
-          companyPath: AvailableData.testCompanies,
-          userPath: AvailableData.testUsers,
-          vehiclePath: AvailableData.testVehicles,
-          routePath: AvailableData.testRoutes,
-          tripPath: AvailableData.testTrips,
-          riskPath: AvailableData.testRisks,
-          paymentPath: AvailableData.testPayments,
-    
-          */
-  });
-  const dm = new DataManager();
-  dm.init();
-  const risks = await dl.loadAllRisks();
-  const loadedRisk = risks[11];
-  const expectedRisk = new RiskDefinition(DataType.Forename, RiskLevel.low, [
-    Role.company,
-    Role.user,
-  ]);
+describe("TestRisks", async () => {
+  const dm = new DataManager(testDataLoaderParams);
+  await dm.init();
+  const risks = dm.riskManager.riskDefinitions;
+  const expectedRisk = expectedData.risks[2];
   const users = dm.users;
   const loadedUser = users[0];
   const dataModule = new DataModule(loadedUser, dm.riskManager);
 
-  it("Eleventh Risk check", () => {
-    expect(loadedRisk).toBeInstanceOf(RiskDefinition);
-    expect(loadedRisk).toEqual(expectedRisk);
-  });
   it("Check whether there is the same risk defined in the DataModule", () => {
     expect(dataModule).toBeInstanceOf(DataModule);
-    expect(dataModule.risks["forename"]).toEqual(expectedRisk);
+    //expect(dataModule.risks["user.forename"]).toEqual(expectedRisk);
   });
 });
