278f026e968a8c8ba2965ffb923e642e71e9e72b
DanialAhmed <danialahmed946@gmail.com>
2022-06-26 15:54:51 +0200

    fix: refactor tests

--- src/backend/__tests__/RiskManager.spec.ts
+++ src/backend/__tests__/RiskManager.spec.ts
@@ -8,7 +8,6 @@ import { testDataLoaderParams } from "./DataLoader.spec";
 describe("TestRisks", async () => {
   const dm = new DataManager(testDataLoaderParams);
   await dm.init();
-  const risks = dm.riskManager.riskDefinitions;
   const expectedRisk = expectedData.risks[2];
   const users = dm.users;
   const loadedUser = users[0];
@@ -16,6 +15,5 @@ describe("TestRisks", async () => {
 
   it("Check whether there is the same risk defined in the DataModule", () => {
     expect(dataModule).toBeInstanceOf(DataModule);
-    //expect(dataModule.risks["user.forename"]).toEqual(expectedRisk);
   });
 });
