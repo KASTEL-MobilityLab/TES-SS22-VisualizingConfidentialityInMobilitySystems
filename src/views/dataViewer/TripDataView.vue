ef63c778c7fcf4eb23a697d13653cb0c270a2987
DanialAhmed <danialahmed946@gmail.com>
2022-06-26 15:59:57 +0200

    fix: refactor TripDataView

--- src/views/dataViewer/TripDataView.vue
+++ src/views/dataViewer/TripDataView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import type { DataManager } from "@/backend/DataManager";
 import { DataModule } from "@/backend/dataModules/DataModule";
 import { dataManagerKey } from "@/keys";
 import { inject } from "vue";
 
-const $dm = inject(dataManagerKey);
+const $dm = inject(dataManagerKey) as DataManager;
 var dmUsers = $dm?.users;
-var dmDataModule = new DataModule(dmUsers![0], $dm?.riskManager);
+var dmDataModule = new DataModule(dmUsers![0], $dm.riskManager);
 </script>
 
 <template>
