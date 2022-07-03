52dc196ff8a0df4f1aa53dc5c1089c982f5388a5
Moritz Mistol <moritz.mistol@gmail.com>
2022-07-03 14:19:59 +0200

    chore: remove event listener from modal

--- src/components/ExplanationModal.vue
+++ src/components/ExplanationModal.vue
@@ -1,36 +1,4 @@
-<script setup lang="ts">
-import { onMounted } from "vue";
-
-onMounted(() => {
-  /**
-   * How do we update the modal based on which button is clicked?
-   * Bootstrap suggests using the `data-bs-*` attributes (like it is implemented now)
-   * and update the modal with the event listener below
-   * This is pretty ugly.
-   *
-   * One solution is to store the currently selected DataType as an property
-   * in the data manager, so that instead of getting the clicked button attributes,
-   * we could request the appropriate Risk Instance from the data manager.
-   * But this is not ideal as well.
-   */
-  const explanationModal = document.getElementById(
-    "explanationModal"
-  ) as HTMLElement;
-  explanationModal.addEventListener("show.bs.modal", function (event: Event) {
-    // Button that triggered the modal
-    const button = (event as MouseEvent).relatedTarget as HTMLButtonElement;
-    // Extract info from data-bs-* attributes
-    const recipient = button.getAttribute("data-bs-origin");
-
-    // update the content of the modal
-    const modalTitle = explanationModal.querySelector(".modal-title");
-    const modalBody = explanationModal.querySelector(".modal-body");
-    if (modalTitle && modalBody) {
-      modalBody.textContent = recipient;
-    }
-  });
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div id="explanationModal" class="modal" tabindex="-1">
