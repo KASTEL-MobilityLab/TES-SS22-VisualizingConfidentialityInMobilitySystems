831a55f8f7a02e1bc782c62b1d553854c90284d0
Moritz Mistol <moritz.mistol@gmail.com>
2022-07-04 17:37:53 +0200

    feat: add explanationCard component

---
+++ src/components/ExplanationCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  subtitle?: string;
+  content: string;
+}>();
+</script>
+
+<template>
+  <div class="card" style="width: 18rem">
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <h6 v-if="subtitle" class="card-subtitle mb-2 text-muted">
+        {{ subtitle }}
+      </h6>
+      <p class="card-text">
+        {{ content }}
+      </p>
+    </div>
+  </div>
+</template>
