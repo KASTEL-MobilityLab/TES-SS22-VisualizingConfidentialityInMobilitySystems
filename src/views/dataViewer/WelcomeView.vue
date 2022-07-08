53ddcba84acb44e76d50b73cd5d4d304d32de7ab
Moritz Mistol <moritz.mistol@gmail.com>
2022-07-08 18:50:35 +0200

    feat: edit the welcome view

--- src/views/dataViewer/WelcomeView.vue
+++ src/views/dataViewer/WelcomeView.vue
@@ -1,11 +1,17 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="container text-center">
-    <h3>{{ $t("app.welcome") }}</h3>
+    <h3>{{ $t("app.welcome.title") }}</h3>
 
-    <span>
-      If you need help, click on the Help button in the top right corner.
-    </span>
+    <nav class="flex-column lead">
+      <p>
+        {{ $t("app.welcome.getting_started") }}
+      </p>
+      <p>
+        {{ $t("app.welcome.click_to_see_data") }}
+      </p>
+      <p>
+        {{ $t("app.welcome.get_help") }}
+      </p>
+    </nav>
   </div>
 </template>
