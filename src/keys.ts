7c1b055c536938cc096200fe614150ce17929360
Moritz Mistol <moritz.mistol@gmail.com>
2022-07-12 16:47:56 +0200

    chore: remove unknown conversion of symbol

--- src/keys.ts
+++ src/keys.ts
@@ -1,6 +1,6 @@
 import type { InjectionKey, Ref } from "vue";
 import type { DataManager } from "./backend/DataManager";
 
-export const dataManagerKey = (<unknown>Symbol("$dataManager")) as InjectionKey<
+export const dataManagerKey = Symbol("$dataManager") as InjectionKey<
   Ref<DataManager>
 >;
