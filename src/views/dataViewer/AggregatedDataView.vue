db8d5b41e9fcffc0c016fce43b3513706cec7031
DanialAhmed <danialahmed946@gmail.com>
2022-07-27 16:32:25 +0200

    feat: dispaly aggregated data

--- src/views/dataViewer/AggregatedDataView.vue
+++ src/views/dataViewer/AggregatedDataView.vue
@@ -2,7 +2,8 @@
 import type { DataManager } from "@/backend/DataManager";
 import { dataManagerKey } from "@/keys";
 import { inject, type Ref } from "vue";
-
+const TIME_METRIC = "Minutes";
+const PRICE_METRIC = "EUR";
 const $dm = inject(dataManagerKey) as Ref<DataManager>;
 </script>
 
@@ -14,9 +15,39 @@ const $dm = inject(dataManagerKey) as Ref<DataManager>;
       <p>
         {{ $t("app.aggregatedDataReasoning") }}
       </p>
-      <p>
-        {{ $dm.aggregatedData.averagePrice }}
-      </p>
+
+      <div class="row m-2 p-2">
+        <div class="col m-2 fw-bold">
+          <div class="keyValue__key">
+            {{ $t("app.aggregatedData.numberOfActiveVehicles") }}
+          </div>
+        </div>
+        <div class="keyValue__value">
+          {{ $dm.aggregatedData.numberOfActiveVehicles }}
+        </div>
+      </div>
+
+      <div class="row m-2 p-2">
+        <div class="col m-2 fw-bold">
+          <div class="keyValue__key">
+            {{ $t("app.aggregatedData.averageDuration") }}
+          </div>
+        </div>
+        <div class="keyValue__value">
+          {{ $dm.aggregatedData.averageDuration }} {{ TIME_METRIC }}
+        </div>
+      </div>
+
+      <div class="row m-2 p-2">
+        <div class="col m-2 fw-bold">
+          <div class="keyValue__key">
+            {{ $t("app.aggregatedData.averagePrice") }}
+          </div>
+        </div>
+        <div class="keyValue__value">
+          {{ $dm.aggregatedData.averagePrice }} {{ PRICE_METRIC }}
+        </div>
+      </div>
     </nav>
   </div>
 </template>
