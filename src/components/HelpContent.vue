4987a6612488bf74eec8c0afe01114e64ec9b9be
Thomas Manogg <thomasmanogg@yahoo.de>
2022-08-05 15:56:24 +0200

    feat: adds translation and refactors help page

--- src/components/HelpContent.vue
+++ src/components/HelpContent.vue
@@ -1,50 +1,32 @@
 <script setup lang="ts">
-import { getTranslationKeyForExplanation } from "@/utils/translationUtils";
-import { computed, type ComputedRef, type Ref } from "@vue/reactivity";
-import { inject } from "vue";
 import { useI18n } from "vue-i18n";
-import HelpCard from "./HelpCard.vue";
-import HelpModal from "./HelpModal.vue";
+import InfoCard from "./InfoCard.vue";
 
-// must use this if we want to use the translation function inside script setup
 const { t } = useI18n();
-
-const applicationGoalTitel: ComputedRef<string> = computed(() => {
-  return t("About the App");
-});
-
-const applicationGoal: ComputedRef<string> = computed(() => {
-  return t("This is the goal of the app");
-});
-
-const howToUseTitel: ComputedRef<string> = computed(() => {
-  return t("How it works");
-});
-
-const howToUse: ComputedRef<string> = computed(() => {
-  return t("This is how you use the app");
-});
-
-const availableInfoTitel: ComputedRef<string> = computed(() => {
-  return t("What infos you can get");
-});
-
-const availableInfo: ComputedRef<string> = computed(() => {
-  return t("These are the infos you can get from the app");
-});
 </script>
-
 <template>
   <div class="container">
     <div class="row">
       <div class="col d-flex align-items-stretch">
-        <HelpCard :title="applicationGoalTitel" :content="applicationGoal" />
+        <InfoCard
+          :title="t('help.about_the_app')"
+          :content="t('help.about_the_app_text')"
+        />
       </div>
-      <div v-if="howToUse" class="col d-flex align-items-stretch">
-        <HelpCard :title="howToUseTitel" :content="howToUse" />
+      <div v-if="t('help.howToUse')" class="col d-flex align-items-stretch">
+        <InfoCard
+          :title="t('help.how_it_works')"
+          :content="t('help.how_it_works_text')"
+        />
       </div>
-      <div v-if="availableInfo" class="col d-flex align-items-stretch">
-        <HelpCard :title="availableInfoTitel" :content="availableInfo" />
+      <div
+        v-if="t('help.what_infos_you_can_get')"
+        class="col d-flex align-items-stretch"
+      >
+        <InfoCard
+          :title="t('help.what_infos_you_can_get')"
+          :content="t('help.what_infos_you_can_get_text')"
+        />
       </div>
     </div>
   </div>
--- src/i18n.ts
+++ src/i18n.ts
@@ -2,13 +2,11 @@ import de from "@/locales/de.json";
 import en from "@/locales/en.json";
 import explanationsDE from "@/locales/explanations/explanationsDE.json";
 import explanationsEN from "@/locales/explanations/explanationsEN.json";
-import helpDE from "@/locales/help/helpDE.json";
-import helpEN from "@/locales/help/helpEN.json";
 import { createI18n } from "vue-i18n";
 
 // merge explanations into locales (target is en)
-Object.assign(en, explanationsEN, helpEN);
-Object.assign(de, explanationsDE, helpDE);
+Object.assign(en, explanationsEN);
+Object.assign(de, explanationsDE);
 
 export const i18n = createI18n({
   legacy: false,
