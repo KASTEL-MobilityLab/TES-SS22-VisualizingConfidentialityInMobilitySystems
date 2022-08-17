53ddd156ca99926664a0936aec5772dff1c54a30
Moritz Mistol <moritz.mistol@gmail.com>
2022-08-17 16:48:29 +0200

    Revert "fix: add support for fetching route in node"
    This reverts commit 78046179ade677fb7c703914013dc4c32fb98837.

--- src/backend/utils/Routing.ts
+++ src/backend/utils/Routing.ts
@@ -1,4 +1,4 @@
-import { fetch } from "cross-fetch";
+import { isNode } from "browser-or-node";
 import type { Route } from "../dataFields";
 import { LatLng } from "./LatLng";
 
@@ -13,6 +13,9 @@ export async function fetchWaypoints(
   route: Route,
   profile: "driving" | "cycling" = "driving"
 ): Promise<LatLng[]> {
+  if (isNode) {
+    throw new Error("Fetching from node environment currently not supported.");
+  }
   if (!route.waypoints) {
     // request the waypoints for this route and set them in the route instance
     const waypoints = await fetchDirectionsAPI(route.start, route.end, profile);
