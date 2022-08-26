05f6c62e54827b70e03b275d23ba65af33be9a1f
Moritz Mistol <moritz.mistol@gmail.com>
2022-08-26 12:43:57 +0200

    feat: add print data utility

---
+++ src/utils/RandomDataPrinter.ts
@@ -0,0 +1,77 @@
+import { Route, type DataField } from "@/backend/dataFields";
+import type { LatLng as CustomLatLng } from "@/backend/utils/LatLng";
+import { RandomDataGenerator } from "@/backend/utils/RandomDataGenerator";
+import { instanceToPlain } from "class-transformer";
+
+/**
+ * Prints the specified data to the console in a JSON-Ready format.
+ * This is basically a wrapper for the RandomDataGenerator utility.
+ */
+export class RandomDataPrinter {
+  private randomDataGenerator: RandomDataGenerator;
+
+  private clickedPositions: CustomLatLng[] = [];
+
+  /**
+   * Constructs a new RandomDataPrinter with the specified RandomDataGenerator.
+   */
+  constructor(randomDataGenerator?: RandomDataGenerator) {
+    this.randomDataGenerator = randomDataGenerator || new RandomDataGenerator();
+  }
+
+  /**
+   * Prints the specified data (must extend DataField) to the console in a JSON-Ready format.
+   * @param data The Array of instances of a DataField Subclass to print.
+   */
+  private print<T extends DataField>(data: T[]): void {
+    console.log(JSON.stringify(instanceToPlain(data), null, 4));
+  }
+
+  /**
+   * Prints a number of random users to the console in a JSON-Ready format.
+   *
+   * @param count The number of random users to generate.
+   * @param startId the starting id for the first user.
+   */
+  public printUsers(count: number, startId: number) {
+    const randomUsers = this.randomDataGenerator.generateUsers(count, startId);
+    this.print(randomUsers);
+  }
+
+  /**
+   * Adds a new waypoint to the current route.
+   *
+   * @param waypoint The waypoint to add to the current route.
+   */
+  public addWayPointToCurrentRoute(waypoint: CustomLatLng) {
+    this.clickedPositions.push(waypoint);
+  }
+
+  /**
+   * Prints the current route to the console in a JSON-Ready format.
+   */
+  public printCurrentRoute() {
+    const len = this.clickedPositions.length;
+    if (len > 1) {
+      const start = this.clickedPositions[0];
+      const end = this.clickedPositions[len - 1];
+      const route = new Route("R0X", start, end, this.clickedPositions);
+      this.print([route]);
+    } else if (len === 1) {
+      console.log(JSON.stringify(instanceToPlain(this.clickedPositions[0])));
+    }
+  }
+
+  /**
+   * Prints a number of random routes to the console in a JSON-Ready format.
+   *
+   * @param startId the Id of the individual route
+   */
+  public printIndividualRoutes(count: number, startId: number) {
+    const routes = this.randomDataGenerator.generateIndividualRoutes(
+      count,
+      startId
+    );
+    this.print(routes);
+  }
+}
