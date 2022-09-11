595aa74411b9adc76c01fffc5c92656c3ad86ab1
Thomas Manogg <thomasmanogg@yahoo.de>
2022-09-11 22:15:47 +0200

    feat: implement buttons for default view

--- src/views/dataViewer/DefaultView.vue
+++ src/views/dataViewer/DefaultView.vue
@@ -4,32 +4,72 @@ import { RouterLink } from "vue-router";
 
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col m-5">
-        <RouterLink to="rider" class="link-dark">{{
-          $t("app.dataViews.riderDataView")
-        }}</RouterLink>
+    <div class="row m-5">
+      <div class="col">
+        <RouterLink to="rider">
+          <button
+            type="button"
+            class="btn btn-dark btn-lg"
+            style="
+              --bs-btn-padding-y: 1rem;
+              --bs-btn-padding-x: 2rem;
+              --bs-btn-font-size: 1.2rem;
+            "
+          >
+            <i class="fa-solid fa-id-card"></i>
+            {{ $t("app.dataViews.riderDataView") }}
+          </button>
+        </RouterLink>
+      </div>
+      <div class="col">
+        <RouterLink to="payment" class="link-dark">
+          <button
+            type="button"
+            class="btn btn-dark btn-lg"
+            style="
+              --bs-btn-padding-y: 1rem;
+              --bs-btn-padding-x: 2rem;
+              --bs-btn-font-size: 1.2rem;
+            "
+          >
+            <i class="fa-solid fa-sack-dollar"></i>
+            {{ $t("app.dataViews.paymentDataView") }}
+          </button>
+        </RouterLink>
       </div>
     </div>
-    <div class="row">
-      <div class="col m-5">
-        <RouterLink to="payment" class="link-dark">{{
-          $t("app.dataViews.paymentDataView")
-        }}</RouterLink>
+    <div class="row m-5">
+      <div class="col">
+        <RouterLink to="vehicle" class="link-dark">
+          <button
+            type="button"
+            class="btn btn-dark btn-lg"
+            style="
+              --bs-btn-padding-y: 1rem;
+              --bs-btn-padding-x: 2rem;
+              --bs-btn-font-size: 1.2rem;
+            "
+          >
+            <i class="fa-solid fa-car"></i>
+            {{ $t("app.dataViews.vehicleDataView") }}
+          </button>
+        </RouterLink>
       </div>
-    </div>
-    <div class="row">
-      <div class="col m-5">
-        <RouterLink to="vehicle" class="link-dark">{{
-          $t("app.dataViews.vehicleDataView")
-        }}</RouterLink>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col m-5">
-        <RouterLink to="trip" class="link-dark">{{
-          $t("app.dataViews.tripDataView")
-        }}</RouterLink>
+      <div class="col">
+        <RouterLink to="trip" class="link-dark">
+          <button
+            type="button"
+            class="btn btn-dark btn-lg"
+            style="
+              --bs-btn-padding-y: 1rem;
+              --bs-btn-padding-x: 2rem;
+              --bs-btn-font-size: 1.2rem;
+            "
+          >
+            <i class="fa-solid fa-road"></i>
+            {{ $t("app.dataViews.tripDataView") }}
+          </button>
+        </RouterLink>
       </div>
     </div>
   </div>
